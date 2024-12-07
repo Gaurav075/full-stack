1893dd3e22ca5458777b2f42aa69428af847fac9
Gaurav075 <125338113+Gaurav075@users.noreply.github.com>
2024-12-07 14:22:11 +0000

    done

---
+++ Backend/server.js
@@ -0,0 +1,43 @@
+import express from 'express';
+
+const app = express();
+// app.get('/',(req,res)=>{
+//     res.send('Server is ready');
+// });
+
+app.get('/api/jokes',(req,res)=>{
+    const jokes=[
+        {
+            id:1,
+            title:'A joke',
+            content:'This is a joke'
+        },
+        {
+            id:2,
+            title:'Aother joke',
+            content:'This is another joke'
+        },
+        {
+            id:3,
+            title:'third joke',
+            content:'This is third joke'
+        },
+        {
+            id:4,
+            title:'fourth joke',
+            content:'This is fourth joke'
+        },
+        {
+            id:5,
+            title:'fifth joke',
+            content:'This is fifth joke'
+        },
+    ];
+    res.send(jokes);
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port,()=>{
+    console.log(`Serve at http://localhost:${port}`);
+    
+})
